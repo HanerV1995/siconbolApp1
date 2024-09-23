ab0ef8bb9615f77ea683390dbe35039027fb9b91
HanerV1995 <hanervalencia@hotmail.es>
2024-09-22 19:50:07 -0500

    Proyecto Base

---
+++ src/main/java/com/siconbolApp1/SiconbolApplication.java
@@ -0,0 +1,13 @@
+package com.siconbolApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiconbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiconbolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siconbolApp1/SiconbolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siconbolApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiconbolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
